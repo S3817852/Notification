ea9b4c29823daf527cbec8f70de62e8758d19d9f
S3817852 <57226212+S3817852@users.noreply.github.com>
2021-04-28 00:10:02 +0700

    Add files via upload

---
+++ website/urls.py
@@ -0,0 +1,38 @@
+"""website URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views
+
+from core.views import frontpage, signup
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('hello/', include('hello.urls')),
+    
+    path('product/', include('owner.urls')),
+    path('create/', include('owner.urls')),
+    path('details/', include('owner.urls')),
+    path('delete/', include('owner.urls')),
+    # path('', include('home.urls')),
+    path('notifications/', include('notification.urls')),
+    path('job/', include('job.urls')),
+    path('dashboard/', include('userprofile.urls')),
+    path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login')
+]
